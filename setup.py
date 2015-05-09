878454b133d87265eaa2784e90582b48d41e406e
Thomas Levine <_@thomaslevine.com>
2015-05-09 09:06:58 +0000

    setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(name='usace_public_notices',
+      author='Thomas Levine',
+      author_email='_@thomaslevine.com',
+      description='United States Army Corps of Engineers Public Notices',
+      url='http://scott.dada.pink',
+      packages=['usace_public_notices'],
+      entry_points = {'console_scripts': ['usace-public-notices = usace_public_notices:cli']},
+      install_requires = ['requests', 'vlermv>=1.4.0'],
+      tests_require = ['pytest>=2.6.4'],
+      version='0.2.0',
+      license='AGPL',
+)
