dced6251f170a00ef3723d82ae6c8f15f8e7b007
Thomas Levine <_@thomaslevine.com>
2015-05-09 09:04:18 +0000

    -

--- usace_public_notices/__init__.py
+++ usace_public_notices/__init__.py
@@ -2,10 +2,25 @@ import datetime
 
 from . import seed, download, parse
 
-def public_notices(sites = seed.sites_short, today = datetime.date.today()):
-    for site in sites():
+def public_notices(sites = seed.sites_medium, today = datetime.date.today()):
+    for site in sites:
         for link in parse.feed(download.feed(today, site)):
             record = parse.summary(download.summary(link))
             xs = record.pop('attachments')
-            args = record, (parse.attachment(download.attachment(x)) for x in xs)
-            yield record['article_id'], args
+            yield record, (parse.attachment(download.attachment(x)) for x in xs)
+
+def cli():
+    from concurrent.futures import ThreadPoolExecutor
+    import sys
+    threads = 15
+
+    def f(site):
+        try:
+            for record, attachments in public_notices(sites = [site]):
+                sys.stdout.write(record['title'] + '\n')
+        except:
+            sys.stderr.write('Aborting site number %d because of an exception\n' % site)
+
+    with ThreadPoolExecutor(threads) as e:
+        for site in seed.sites_medium():
+            e.submit(f, site)
--- usace_public_notices/seed.py
+++ usace_public_notices/seed.py
@@ -1,9 +1,8 @@
-def districts():
-    return ['sam', 'mvn']
+#: Just Alabama and Louisiana
+sites_short = [417, 461]
 
-def sites_short():
-    'Just Alabama and Louisiana'
-    return [417, 461]
+#: A wide net
+sites_long = range(800)
 
-def sites_long():
-    return range(800)
+#: These are sites that have turned out to have any notices at all in them.
+sites_medium = range(414, 477)
